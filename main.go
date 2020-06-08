0e88dd60db562a1ac848a9aad579cabcf4a72bd0
iceboy <morfanbk@gmail.com>
2020-06-08 23:30:45 +0800

    更新列示程序

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"ges"
 	"net/http"
 )
 
 func main() {
 	r := ges.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *ges.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello ges</h1>")
 	})
+	r.GET("/hello", func(c *ges.Context) {
+		// expect /hello?name=gesktutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *ges.Context) {
+		c.JSON(http.StatusOK, ges.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
 	r.RUN(":9999")
 }
