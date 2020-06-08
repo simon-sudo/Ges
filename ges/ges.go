88c96684bd96e46df618da15770cde44960686db
iceboy <morfanbk@gmail.com>
2020-06-08 23:44:14 +0800

    分离 router

--- ges/ges.go
+++ ges/ges.go
@@ -1,6 +1,7 @@
 package ges
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,47 +10,37 @@ type HandlerFunc func(*Context)
 
 // Engine 实现 serveHTTP 接口
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 //New ges.Engine的构造函数（constructor)
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 // 添加到路由表
-func (engine Engine) addRouter(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	engine.router.addRoute(method, pattern, handler)
 }
 
 //GET 定义了添加get请求的方法
-func (engine Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRouter("GET", pattern, handler)
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
 }
 
 //POST 定义了添加post的方法
 func (engine Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRouter("POST", pattern, handler)
+	engine.addRoute("POST", pattern, handler)
 }
 
 //RUN 定义启动http服务的方法
-func (engine Engine) RUN(addr string) (err error) {
+func (engine Engine) Run(addr string) (err error) {
 	//cannot use engine (variable of type Engine) as http.Handler value in argument to http.ListenAndServe: missing method ServeHTTP
 	return http.ListenAndServe(addr, engine)
 }
 
-func (engine Engine) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(c)
-	} else {
-		c.String(http.StatusNotFound, "404:%s\n", c.Path)
-	}
-
-}
-
 func (engine Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
-	engine.handle(c)
+	engine.router.handle(c)
 }
